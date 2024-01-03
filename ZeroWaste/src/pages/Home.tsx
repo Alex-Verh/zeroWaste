16ab34a3b042e3bf447bc5a8e07a96799af5a1b8
Alex-Verh <alexandrverhovetchi@gmail.com>
2024-01-03 20:21:56 +0200

    Initialized the project and Created the first intro page

---
+++ ZeroWaste/src/pages/Home.tsx
@@ -0,0 +1,54 @@
+import { IonContent, IonHeader, IonPage, IonCard, IonImg, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage >
+      <IonHeader>
+
+      </IonHeader>
+      <IonContent fullscreen>
+        <div className='custom-background'>
+        <IonCard className="custom-card">
+          <IonImg src="/assets/main_1.jpg"></IonImg>
+          <IonCardHeader>
+            <IonCardTitle className="custom-title">Groceries List</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent className="custom-text">
+            <p>
+            Here you can create your own grocery list with help of smart recommendation.
+            </p>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard className="custom-card">
+          <IonImg src="/assets/main_2.jpg"></IonImg>
+          <IonCardHeader>
+            <IonCardTitle className="custom-title">Current Products</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent className="custom-text">
+            <p>
+            Manage your products and be aware of wastage risks.
+            </p>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard className="custom-card">
+          <IonImg src="/assets/main_3.jpg"></IonImg>
+          <IonCardHeader>
+            <IonCardTitle className="custom-title">Donate Food</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent className="custom-text">
+            <p>
+            Reuse your wastage and excesses in effective way.
+            </p>
+          </IonCardContent>
+        </IonCard>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
